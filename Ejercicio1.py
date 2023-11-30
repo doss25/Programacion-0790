4df9bc34845a5bfb16c1f07163e1e448ee2ad8e9
David Solorzano <152554646+doss25@users.noreply.github.com>
2023-11-30 17:36:05 -0400

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,11 @@
+#Practica 1.....
+
+print("Ingrese el primer número entero: ")
+n1 = int(input())
+print("Ingrese el segundo número entero: ")
+n2 = int(input())
+print("Ingrese el tercer número entero: ")
+n3 = int(input())
+list = [n1, n2, n3]
+list.sort()
+print(list)
---
+++ Ejercicio2.py
@@ -0,0 +1,6 @@
+#Practica 2.....
+
+variable = input("Escriba una frase en mayúscula")
+variable_minuscula = variable.lower()
+palabras_variable = variable_minuscula.split()
+print(palabras_variable)
